e4311e0f5b609443cac4a2b65393a7af57f64396
natefrezzell <nateb9086@gmail.com>
2026-04-14 12:21:45 -0400

    Just need to know whats greater than or less than

--- src/main/java/Highscore/Score.java
+++ src/main/java/Highscore/Score.java
@@ -1,4 +1,37 @@
 package Highscore;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Score {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        System.out.println("Score for Sheets My Wife ");
+        int teamscore1 = sc.nextInt();
+        System.out.println("Score for Wawa Venus");
+        int teamscore2 = sc.nextInt();
+
+        String input = ("Sheets My Wife:Wawa Venus|" + teamscore1 + ":" + teamscore2);
+        String[] splitInput = input.split(Pattern.quote( "|"));
+        String scores = splitInput[1];
+        String[] splitScores = splitInput[1].split(Pattern.quote(":"));
+
+        int finalscore1 = Integer.parseInt(splitScores[0]);
+        int finalscore2 = Integer.parseInt(splitScores[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
